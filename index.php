ada62096ae138c4b6b44c4bafd7efe4ecc8a4105
Spectrum5 <toskusergio@gmail.com>
2023-02-21 23:48:05 +0100

    Aggiunto form aggiunta numero caratteri

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 
 <html lang="it">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- Bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>PHP Strong Password Generator</title>
-    </head>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-    <body>
-        
-    </body>
+    <title>PHP Strong Password Generator</title>
+</head>
+
+<body>
+    <section class="bg-success d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div class="container bg-light py-4">
+            <form action="" method="get" class="d-flex justify-content-center">
+                <label for="password-length" class="pe-4">Inserisci numero caratteri della password:</label>
+                <input name="password-length" type="text">
+                <button type="submit">Crea</button>
+            </form>
+        </div>
+    </section>
+</body>
+
 </html>
